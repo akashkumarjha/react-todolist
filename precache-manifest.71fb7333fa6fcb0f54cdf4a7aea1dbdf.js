95beeba24d779d2d59a029deeb328f0c73cdf727
akashkumarjha <akashjha513@gmail.com>
2020-09-20 22:00:47 +0530

    Updates

---
+++ precache-manifest.71fb7333fa6fcb0f54cdf4a7aea1dbdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41de9280a918d510fb74a2fd166e929",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "8d50f8a574312e33a407",
+    "url": "/react-todolist/static/css/main.7d8fef2e.chunk.css"
+  },
+  {
+    "revision": "35ad13d79940f688615e",
+    "url": "/react-todolist/static/js/2.fddab1d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.fddab1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d50f8a574312e33a407",
+    "url": "/react-todolist/static/js/main.b696b614.chunk.js"
+  },
+  {
+    "revision": "dea162c310a378ff9d0d",
+    "url": "/react-todolist/static/js/runtime-main.82f4965a.js"
+  }
+]);
